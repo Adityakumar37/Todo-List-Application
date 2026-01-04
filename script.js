406a07642fb731a0e22487a22c9c32bd97c4296f
Aditya <amanssm221@gmail.com>
2026-01-04 20:44:41 +0530

    added alert text

--- script.js
+++ script.js
@@ -4,15 +4,24 @@
 
 
 function tolist(){
+
     let task=document.getElementById("taskinput").value;
 
     let tasklist=document.getElementById('tasklist')
     let li=document.createElement("li");
+
+    if (task==""){
+        alert("Add item ")
+    }
     li.textContent = task;
     tasklist.appendChild(li);
     document.getElementById("taskinput").value = "";
 
 
 }
+
+let rem=document.getElementById('clearll')
+
+
 let btn=document.getElementById('addtask')
 btn.addEventListener("click",tolist)
